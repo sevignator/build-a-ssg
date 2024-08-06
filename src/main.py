b5ac0059a3414759cc50cbc29bd864f04996a3e8
Mikaël Sévigny <sevignator@hotmail.com>
2024-08-05 21:01:27 -0400

    Add solution for 05-01

--- src/main.py
+++ src/main.py
@@ -1,27 +1,60 @@
-from block import markdown_to_html_node
+import os
+import shutil
 
 
 def main() -> None:
-    text = """# This is a heading
+    current_dir = os.path.dirname(__file__)
+    root_dir = os.path.dirname(current_dir)
+    source_dir = os.path.join(root_dir, "static")
+    dest_dir = os.path.join(root_dir, "public")
 
-    This is a paragraph of text. It has some **bold** and *italic* words inside of it.
+    copy_source_to_dest(source_dir, dest_dir)
 
-    > Here's a quote for ya.
 
-    * This is the first list item in a list block
-    * This is a list item
-    * This is another list item
+def copy_source_to_dest(src_dir: str, dest_dir: str):
+    if not os.path.exists(src_dir):
+        raise ValueError(f"Source directory doesn't exist: {src_dir}.")
 
-    ```
-    Some code goes here
-    ```
+    if not os.path.exists(dest_dir):
+        raise ValueError(f"Destination directory doesn't exist: {dest_dir}.")
 
-    1. An ordered list
-    2. Is used to display items
-    3. In a sequence
-    """
-    html = markdown_to_html_node(text)
-    print(html)
+    # Get the list of all entries in both directories
+    src_content = os.listdir(src_dir)
+    dest_content = os.listdir(dest_dir)
+
+    # Exit early if the source directory has no entry
+    if len(src_content) == 0:
+        return
+
+    # Clear destination directory if non-empty
+    if len(dest_content) != 0:
+        for entry in dest_content:
+            dest_entry_path = os.path.join(dest_dir, entry)
+
+            # Delete files
+            if os.path.isfile(dest_entry_path):
+                os.remove(dest_entry_path)
+                continue
+
+            # Delete directory trees
+            shutil.rmtree(dest_entry_path)
+
+    # For each entry, determine whether it's a file or a sub-directory
+    for entry in src_content:
+        src_entry_path = os.path.join(src_dir, entry)
+        dest_entry_path = os.path.join(dest_dir, entry)
+
+        # Copy the file over to the destination and move to the next iteration
+        if os.path.isfile(src_entry_path):
+            print(f"New file: {dest_entry_path}")
+            shutil.copy(src_entry_path, dest_entry_path)
+            continue
+
+        # Create same directory in the destination and recursively
+        # call this function.
+        print(f"New directory: {dest_entry_path}")
+        os.mkdir(dest_entry_path)
+        copy_source_to_dest(src_entry_path, dest_entry_path)
 
 
 main()
